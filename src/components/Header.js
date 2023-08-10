3c3be6401938d8d6f4e55d03312d7afad950e37a
Jair Gomez <jairherber505@gmail.com>
2023-08-09 22:32:42 -0700

    basic app

---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      <h1>Full Stack Developer</h1>
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>Projects</li>
+          <li>About Me</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
+
+export default Header;
