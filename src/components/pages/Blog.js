09a3bb936b35f2efb2af1001075e9cce9433bb83
Jair Gomez <jairherber505@gmail.com>
2023-08-18 17:31:58 -0700

    added the contact page

--- src/components/pages/Blog.js
+++ src/components/pages/Blog.js
@@ -1,24 +1,29 @@
-import React from 'react';
+import React from "react";
 
 export default function Blog() {
   return (
-    <div>
-      <h1>Blog Page</h1>
-      <p>
-        Donec a volutpat quam. Curabitur nec varius justo, sed rutrum ligula.
-        Curabitur pellentesque turpis sit amet eros iaculis, a mollis arcu
-        dictum. Ut vel ante eget massa ornare placerat. Etiam nisl orci, finibus
-        sodales volutpat et, hendrerit ut dolor. Suspendisse porta dictum nunc,
-        sed pretium risus rutrum eget. Nam consequat, ligula in faucibus
-        vestibulum, nisi justo laoreet risus, luctus luctus mi lacus sit amet
-        libero. Class aptent taciti sociosqu ad litora torquent per conubia
-        nostra, per inceptos himenaeos. Mauris pretium condimentum tellus eget
-        lobortis. Interdum et malesuada fames ac ante ipsum primis in faucibus.
-        Donec placerat accumsan mi, ut congue neque placerat eu. Donec nec ipsum
-        in velit pellentesque vehicula sit amet at augue. Maecenas aliquam
-        bibendum congue. Pellentesque semper, lectus non ullamcorper iaculis,
-        est ligula suscipit velit, sed bibendum turpis dui in sapien.
-      </p>
+    <div className="form-container">
+      <div className="form">
+        <span className="heading">Get in touch</span>
+        <input placeholder="Name" type="text" className="input" />
+        <input placeholder="Email" id="mail" type="email" className="input" />
+        <textarea
+          placeholder="Say Hello"
+          rows="10"
+          cols="30"
+          id="message"
+          name="message"
+          className="textarea"
+        ></textarea>
+        <div className="button-container">
+          <div className="send-button">Send</div>
+          <div className="reset-button-container">
+            <div id="reset-btn" className="reset-button">
+              Reset
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
