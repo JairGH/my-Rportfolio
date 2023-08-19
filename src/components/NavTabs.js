71e58907faff094248882b16c279c3ae59d62cb2
Jair Gomez <jairherber505@gmail.com>
2023-08-18 17:12:14 -0700

    making progress on the portfolio section

--- src/components/NavTabs.js
+++ src/components/NavTabs.js
@@ -4,7 +4,7 @@ import React from "react";
 // We assign them to their own variable names
 function NavTabs({ currentPage, handlePageChange }) {
   return (
-    <ul className="nav nav-tabs d-flex justify-content-end">
+    <ul className="nav nav-tabs d-flex justify-content-end p-5" id="nav">
       <li className="nav-item">
         <a
           href="#home"
@@ -23,7 +23,7 @@ function NavTabs({ currentPage, handlePageChange }) {
           // Check to see if the currentPage is `About`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
           className={currentPage === "About" ? "nav-link active" : "nav-link"}
         >
-          About
+          Portfolio
         </a>
       </li>
       <li className="nav-item">
@@ -33,7 +33,7 @@ function NavTabs({ currentPage, handlePageChange }) {
           // Check to see if the currentPage is `Blog`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
           className={currentPage === "Blog" ? "nav-link active" : "nav-link"}
         >
-          Blog
+          Contact
         </a>
       </li>
       <li className="nav-item">
@@ -43,7 +43,7 @@ function NavTabs({ currentPage, handlePageChange }) {
           // Check to see if the currentPage is `Contact`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
           className={currentPage === "Contact" ? "nav-link active" : "nav-link"}
         >
-          Contact
+          Resume
         </a>
       </li>
     </ul>
