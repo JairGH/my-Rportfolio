70502107c4077afc3b14193cea835c58923a54f3
Jair Gomez <jairherber505@gmail.com>
2023-08-18 17:47:03 -0700

    front-end is done

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,22 +1,28 @@
-import React from 'react';
+import React from "react";
 
 export default function Contact() {
   return (
     <div>
-      <h1>Contact Page</h1>
-      <p>
-        Integer cursus bibendum sem non pretium. Vestibulum in aliquet sem, quis
-        molestie urna. Aliquam semper ultrices varius. Aliquam faucibus sit amet
-        magna a ultrices. Aenean pellentesque placerat lacus imperdiet
-        efficitur. In felis nisl, luctus non ante euismod, tincidunt bibendum
-        mi. In a molestie nisl, eu sodales diam. Nam tincidunt lacus quis magna
-        posuere, eget tristique dui dapibus. Maecenas fermentum elementum
-        faucibus. Quisque nec metus vestibulum, egestas massa eu, sollicitudin
-        ipsum. Nulla facilisi. Sed ut erat ligula. Nam tincidunt nunc in nibh
-        dictum ullamcorper. Class aptent taciti sociosqu ad litora torquent per
-        conubia nostra, per inceptos himenaeos. Etiam ornare rutrum felis at
-        rhoncus. Etiam vel condimentum magna, quis tempor nulla.
-      </p>
+      <h1>Front-end Proficiencies</h1>
+      <ul>
+        <ol>HTML</ol>
+        <ol>CSS</ol>
+        <ol>JavaScript</ol>
+        <ol>JQuery</ol>
+        <ol>Responsive Design</ol>
+        <ol>React</ol>
+        <ol>Bootstrap</ol>
+      </ul>
+      <h1>Back-end Proficiencies</h1>
+      <ul>
+        <ol>APIs</ol>
+        <ol>Node</ol>
+        <ol>Express</ol>
+        <ol>MySQL, Sequelize</ol>
+        <ol>MongoDB</ol>
+        <ol>REST</ol>
+        <ol>GraphQL</ol>
+      </ul>
     </div>
   );
 }
